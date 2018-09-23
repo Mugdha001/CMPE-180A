8b402112a934d28a7fe02f8a2e2b23b68b730945
Mugdha Wadikar <mugdhawadikar@gmail.com>
2018-09-23 14:42:11 -0700

    Matrix generation

--- matrix_operations.cpp
+++ matrix_operations.cpp
@@ -5,28 +5,43 @@
 #include <algorithm>
 #include <sstream>														//library for stringstream operations
 using namespace std;
+
+void input_matrix(string input_file, vector<vector< int >> &matrix_vector);
+void calculate_multiplication();
+
 int main() 
 { 
-    ifstream file;														//create a input stream for reading a file
-    file.open("input_matrix_1.txt");
+	vector<vector< int >> matrix1, matrix2;
+	string input_file_name1 = "input_matrix_1.txt", input_file_name2 = "input_matrix_2.txt";
+	input_matrix(input_file_name1, matrix1);
+	input_matrix(input_file_name2, matrix2);
+	return 0;
+}
+
+void input_matrix(string input_file, vector<vector< int >> &matrix_vector){
+	ifstream file;														//create a input stream for reading a file
+    file.open(input_file);
     string str;
-    //int found;
-    int row = 0;
-    vector<vector<int> > matrix1;
-    while (getline(file, str))											//run the while loop as long as seperate line/s of text is/are found in the input file.
+    unsigned int q=0;
+    while (getline(file, str))
     {
+		q=0;
+		vector< int > numbers;
 		stringstream ss(str);
-        //vector<int> numbers;
         for(int i = 0; ss >> i; ) {
-			matrix1[row].push_back(i);
-         //numbers.push_back(i);
-         cout << i << " ";
-       }
-	row++;
-		
+        numbers.push_back(i);
+        //cout << i << " ";
+        q++;
+        
+    }
+    matrix_vector.push_back(numbers);
    }
-    //cout<< "no of lines" << lines_in_1;
-    
+ 
+   for (unsigned int i = 0; i <matrix_vector.size( ); i++){	   
+	   for(unsigned int j =0; j<q; j++){  
+		//cout << matrix_vector[i][j];
+}
+cout<<endl;
+}
     file.close();
-    return 0;
 }
