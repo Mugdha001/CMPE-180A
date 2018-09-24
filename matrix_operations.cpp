a046df9e2ee6a847010ac5b5bbffc415fb2b6f84
Mugdha Wadikar <mugdhawadikar@gmail.com>
2018-09-23 18:17:34 -0700

    matrix multiplication implementation

--- matrix_operations.cpp
+++ matrix_operations.cpp
@@ -6,78 +6,63 @@
 #include <sstream>														//library for stringstream operations
 using namespace std;
 
-void input_matrix(string input_file, vector<vector< int >> &matrix, int& row);
-void matrix_multiplication(vector<vector< int >> matrix1, vector<vector< int >> matrix2, int row1, int row2);
+void input_matrix(string input_file, vector<vector< int >> &matrix_vector, unsigned int& col);
+void calculate_multiplication(vector<vector< int >> &matrix_vector_1, vector<vector< int >> &matrix_vector_2, unsigned int col1, unsigned int col2);
 
 int main() 
 { 
-	vector<vector< int >> matrix_1, matrix_2;
-	int row1, row2;
+	vector<vector< int >> matrix1, matrix2;
+	unsigned int col1, col2;
 	string input_file_name1 = "input_matrix_1.txt", input_file_name2 = "input_matrix_2.txt";
-	input_matrix(input_file_name1, matrix_1, row1);
-	input_matrix(input_file_name2, matrix_2, row2);
-	matrix_multiplication(matrix_1, matrix_2, row1, row2);
+	input_matrix(input_file_name1, matrix1, col1);
+	input_matrix(input_file_name2, matrix2, col2);
+	
+	calculate_multiplication(matrix1, matrix2, col1, col2);
 	return 0;
 }
 
-void input_matrix(string input_file, vector<vector< int >> &matrix, int& row){
+void input_matrix(string input_file, vector<vector< int >> &matrix_vector, unsigned int& col){
 	ifstream file;														//create a input stream for reading a file
     file.open(input_file);
     string str;
     
     while (getline(file, str))
-    {
-		row=0;
+    {	
+		if (!str.empty())
+        {
+		col=0;	
 		vector< int > numbers;
 		stringstream ss(str);
         for(int i = 0; ss >> i; ) {
         numbers.push_back(i);
-        row++;
-        }
-    matrix.push_back(numbers);
+        col++;
+        
+    }
+    matrix_vector.push_back(numbers);
    }
-   for (unsigned int i = 0; i <matrix.size( ); i++){	   
-	   for(int j =0; j<row; j++){  
-		  cout << matrix[i][j];
-	  }
+	
+    
 }
-     file.close();
-     
+file.close();
 }
+void calculate_multiplication(vector<vector< int >> &matrix_vector_1, vector<vector< int >> &matrix_vector_2,unsigned int col1, unsigned int col2){
 
-void matrix_multiplication(vector<vector< int >> matrix1, vector<vector< int >> matrix2, int row1, int row2){
-	
-	for (unsigned int i = 0; i <matrix1.size( ); i++){	   
-	   for(int j =0; j<3; j++){  
-		  cout << matrix1[i][j];
-	  }
-}
-	
-			
-	
-		/*for (unsigned int i=0; i<matrix1.size();i++){
-			cout << matrix1.pop_back;
-			
+unsigned int row1 = matrix_vector_1.size( );
+unsigned int row2 = matrix_vector_2.size( );
+int sum = 0;
+unsigned int i=0, j = 0;
+while(i<row1){
+	j=0;
+	while(j<col2){
+		sum=0;
+		for( unsigned int k=0; k<row2 ; k++){
+		sum=sum + matrix_vector_1[i][k] * matrix_vector_2[k][j];
 		}
-		int num1,num2,sum;
-		   for (unsigned int i = 0; i <matrix_vector.size( ); i++){	   
-	   for(unsigned int j =0; j<q; j++){  
-		//cout << matrix_vector[i][j];
+		cout<< sum << " ";
+		j++;
 }
-//cout<<endl;
+cout<<endl<<endl;
+	i++;
+}	
 
-		for (unsigned int i = 0; i < matrix1.size( ); i++){	   
-			for(int j =0; j < row1; j++){  
-				num1 = matrix1[i][j];
-				
-				}
-			}
-		for (unsigned int i = 0; i < matrix2.size( ); i++){	   
-			for(int j =0; j < row2; j++){  
-				num2 = matrix2[i][j];
-				
-				}
-			}*/
-				
-		
 }
