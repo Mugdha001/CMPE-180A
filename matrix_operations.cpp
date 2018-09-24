c91925280b8bfa04b7e31c93a3de834a91b40914
Mugdha Wadikar <mugdhawadikar@gmail.com>
2018-09-23 22:40:00 -0700

    changed file path

--- matrix_operations.cpp
+++ matrix_operations.cpp
@@ -18,9 +18,9 @@ int main()
 { 
 	vector<vector< int >> matrix1, matrix2;								// two matrices
 	unsigned int col1, col2;											// number of columns of the two matrices
-	string input_file_name1 = "input_matrix_1.txt", input_file_name2 = "input_matrix_2.txt";  // input files
+	string input_file_name1 = ".\\input_files\\input1_0.txt", input_file_name2 = ".\\input_files\\input1_1.txt";  // input files
 	
-	//function call
+	//function call/s
 	input_matrix(input_file_name1, matrix1, col1);
 	input_matrix(input_file_name2, matrix2, col2);	
 	calculate_multiplication(matrix1, matrix2, col1, col2);
@@ -52,17 +52,17 @@ void input_matrix(string input_file, vector<vector< int >> &matrix_vector, unsig
 file.close();
 }
 void calculate_multiplication(vector<vector< int >> &matrix_vector_1, vector<vector< int >> &matrix_vector_2,unsigned int col1, unsigned int col2){
-//matrix multiplication required the number of columns of 1st matrix and the number of rows of second matrix to be equal.
+//matrix multiplication requires the number of columns of 1st matrix and the number of rows of second matrix to be equal.
 //it is assumed that this condition holds true for the input files
 unsigned int row1 = matrix_vector_1.size( );							//number of rows in matrix1 of input file 1
 unsigned int row2 = matrix_vector_2.size( );							//number of rows in matrix2 of input file 2
 int sum = 0;															//sum of products in each iterations
 unsigned int i=0, j = 0;												//iterators for row1 and col2 
-while(i<row1){
+while(i<row1){															//iterate till rows in matrix1 exist
 	j=0;
-	while(j<col2){
+	while(j<col2){														//iterate till columns in matrrix2 exist
 		sum=0;
-		for( unsigned int k=0; k<row2 ; k++){							
+		for( unsigned int k=0; k<row2 ; k++){							//iterate
 		sum=sum + matrix_vector_1[i][k] * matrix_vector_2[k][j];		//matrix multiplication and summing
 		}
 		cout<< sum << " ";
