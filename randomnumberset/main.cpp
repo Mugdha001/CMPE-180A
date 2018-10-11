9feed19da1b785152a185a824d65df472ee26ed5
Mugdha Wadikar <mugdhawadikar@gmail.com>
2018-10-11 09:20:44 -0700

    main function

--- randomnumberset/main.cpp
+++ randomnumberset/main.cpp
@@ -3,16 +3,27 @@
 #include "RandomNumberSet.cpp"											
  
 int main(){
-	RandomNumberSet random_obj(6);
-	//RandomNumberBucket bucket2(50);
-	cout << "after reset " << endl;
-	//random_obj.reset();
-	int a =1,b=20;
 	
-	cout << "size " << random_obj.size() << endl;
-	cout << "set bit " <<random_obj.set(a) <<endl;
-	cout << "set bit " <<random_obj.set(b) <<endl;
-	cout << "size " << random_obj.size() <<endl;
+	RandomNumberSet random_obj1(110);
+	cout << "set bit 1 " <<random_obj1.set(1) <<endl;
+	cout << "set bit 20 " <<random_obj1.set(2) <<endl;
+	cout << "set bit 10 " <<random_obj1.set(51) <<endl;
+	cout << "set bit 50 " <<random_obj1.set(79) <<endl;
+	cout << "set bit 10 " <<random_obj1.set(109) <<endl;
+	RandomNumberSet random_obj2(10);
+	cout << "set bit 1 " <<random_obj2.set(3) <<endl;
+	cout << "set bit 20 " <<random_obj2.set(4) <<endl;
+	cout << "set bit 10 " <<random_obj2.set(5) <<endl;
+	cout << "set bit 50 " <<random_obj2.set(6) <<endl;
+	//cout << "set bit 10 " <<random_obj2.set(4) <<endl;
+	cout << "size 1 " <<random_obj1.size() <<endl;
+	cout << "size 2 " <<random_obj2.size() <<endl;
+	
+	//overload operator -
+	cout <<"difference " << random_obj1 - random_obj2;
+	//overload operator <<
+	cout << random_obj1;
+	
 	return 0;
 }
 
