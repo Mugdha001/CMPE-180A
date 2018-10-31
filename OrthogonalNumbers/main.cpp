714fe5409620d16b9efaced1eec3dc2c657d77ab
Mugdha Wadikar <mugdhawadikar@gmail.com>
2018-10-31 00:41:36 -0700

    for testing purpose only

---
+++ OrthogonalNumbers/main.cpp
@@ -0,0 +1,31 @@
+#include "OrthogonalNumbers.cpp"											
+ 
+int main(){
+	
+	//RandomNumberSet random_obj1;
+	//RandomNumberSet random_obj2(5);
+	OrthogonalNumbers random_obj3;
+	//OrthogonalNumbers random_obj4(30,15,24);
+	/*cout << "set bit 99 " <<random_obj1.set(1) <<endl;
+	cout << "set bit 20 " <<random_obj1.set(2) <<endl;
+	cout << "set bit 10 " <<random_obj1.set(3) <<endl;
+	cout << "set bit 50 " <<random_obj1.set(4) <<endl;
+	cout << "set bit 100 " <<random_obj1.set(5) <<endl;*/
+	/*RandomNumberSet random_obj5(60);
+	cout << "set bit 1 " <<random_obj2.set(3) <<endl;
+	cout << "set bit 20 " <<random_obj2.set(4) <<endl;
+	cout << "set bit 10 " <<random_obj2.set(51) <<endl;
+	cout << "set bit 50 " <<random_obj2.set(6) <<endl;
+	//cout << "set bit 10 " <<random_obj2.set(4) <<endl;*/
+	/*cout << "size 1 " <<random_obj1.size() <<endl;
+	cout << "size 2 " <<random_obj2.size() <<endl;*/
+	
+	/*//overload operator -
+	cout <<"difference " << random_obj1 - random_obj2;
+	//overload operator <<
+	cout << random_obj1;
+	cout << random_obj2;*/
+	cout <<random_obj3.generate(6);
+	return 0;
+}
+
